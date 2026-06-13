a8bcc4abca463897c10a78f335ecd4979e3c4c4e
Junior <rodriguez.153941@e.unavarra.es>
2026-06-13 22:58:26 +0200

    [verde] - Añadido codigo minimo para cumplir el test de añadir pokemon sin especificar cantida

--- src/CentroPokemon.php
+++ src/CentroPokemon.php
@@ -10,7 +10,13 @@ class CentroPokemon{
     }
 
     public function procesarInstruccion(string $instruccion): string{
+        $instruccionMinusculas = strtolower($instruccion);
+        $partes = explode(" ",$instruccionMinusculas);
 
+        if($partes[0] === "ingresar"){
+            $hp = $this->pokedex->obtenerHPBase($partes[1]);
+            return "$partes[1] x1 | HP Total: $hp";
+        }
     }
 }
 ?>
