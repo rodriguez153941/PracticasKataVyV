d1dcfb3791abac27667bb679fac47e5efb48c113
Junior <rodriguez.153941@e.unavarra.es>
2026-06-14 18:43:23 +0200

    [refactor] - Implemntado metodo independiente y privado para añadir prodcuto sin indicar la cantidad

--- src/ListaCompra.php
+++ src/ListaCompra.php
@@ -5,12 +5,16 @@ class ListaCompra{
     public function __construct(){
 
     }
+    public function añadirProducto($nombre,$cantidad = 1){
+        return $nombre . " x" . $cantidad;
+    }
+
     public function procesarInstruccion(string $instruccion): string{
         $instruccionMinus = strtolower($instruccion);
         $partes = explode(" ",$instruccionMinus);
 
         if($partes[0]==="añadir"){
-            return $partes[1] . " x1"; 
+            return $this->añadirProducto($partes[1]); 
         }
 
         return "";
