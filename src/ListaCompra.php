2da2c409176262c25b302006e48b1c5d4bfcf6f7
Junior <rodriguez.153941@e.unavarra.es>
2026-06-14 18:27:35 +0200

    [rojo] - Falla test para añadir un producto sin indicar la cantidad del producto

---
+++ src/ListaCompra.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+
+class ListaCompra{
+    public function __construct(){
+
+    }
+    public function procesarInstruccion(): string{
+        return "";
+    }
+}
+
+?>
---
+++ tests/ListaCompraTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Test;
+
+use App\ListaCompra;
+use PHPUnit\Framework\TestCase;
+
+class ListaCompraTest extends TestCase{
+    private ListaCompra $lista;
+    public function setUp(): void{
+        $this->lista = new ListaCompra();
+    }
+
+    public function test_añadir_producto_sin_indicar_cantidad(){
+        //arange
+        
+        //act
+        $resultado = $this->lista->procesarInstruccion("añadir pan");
+        //assert
+        $this->assertEquals("pan x1",$resultado);
+    }
+}
+
+?>
